2f2ef633c119c417952ad70d443f962fbaeec3f7
Paul Bartell <pbartell@amazon.com>
2022-07-05 15:04:21 -0700

    verify_manifest.py: Validate SPDX license tags

--- manifest-verifier/verify_manifest.py
+++ manifest-verifier/verify_manifest.py
@@ -1,118 +1,338 @@
 #!/usr/bin/env python3
+#
+# FreeRTOS Project
+# Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# https://www.FreeRTOS.org
+# https://github.com/FreeRTOS
+#
 
 import os, sys
-from yaml import load
-from yaml import CLoader as Loader
-
-from git import Repo
+import yaml
+import requests
+import git
+import gitdb
 from argparse import ArgumentParser
+from termcolor import cprint
 
-REPO_PATH=''
-
-# List of submodules excluded from manifest.yml file
-IGNORE_SUBMODULES_LIST = []
-
-# Obtain submodule path of all entries in manifest.yml file.
-def read_manifest():
-    dict = {}
-
-    # Read YML file
-    path_manifest = os.path.join(REPO_PATH, 'manifest.yml')
-    assert os.path.exists(path_manifest), 'Missing manifest.yml'
-    with open(path_manifest, 'r') as fp:
-        manifest_data = fp.read()
-    yml = load(manifest_data, Loader=Loader)
-    assert 'dependencies' in yml, 'Manifest YML parsing error'
-
-    # Iterate over all the "dependencies" entries, verify that
-    # each contains entries for the following hierarchy:
-    # name: "<library-name>"
-    # version: "<version>"
-    # repository:
-    #   type: "git"
-    #   url: <library-github-url>
-    #   path: <path-to-submodule-in-repository>
-    #
-    for dep in yml['dependencies']:
-        assert 'version' in dep, "Failed to parse 'version/tag' for submodule"
-        assert 'repository' in dep and 'path' in dep['repository'] and 'url' in dep['repository'], "Failed to parse 'repository' object for submodule"
-        dict[dep['name']] = (dep['repository']['path'], dep['version'])
-
-    return dict
-
-# Generate list of submodules path in repository, excluding the
-# path in IGNORES_SUBMODULES_LIST.
-def get_all_submodules():
-    info_dict = {}
-    repo = Repo(REPO_PATH)
-    for submodule in repo.submodules:
-        path = submodule.abspath.replace(REPO_PATH+'/', '')
-        if path not in IGNORE_SUBMODULES_LIST:
-            #print(path,':',submodule.module().head.commit)
-            info_dict[path] = submodule.module().head.commit
-
-    return info_dict
-
-if __name__ == '__main__':
+def process_commandline_args():
     parser = ArgumentParser(description='manifest.yml verifier')
-    parser.add_argument('--repo-root-path',
-                        type=str,
-                        required=None,
+    parser.add_argument('manifest_path',
+                        metavar='manifest_path',
                         default=os.getcwd(),
-                        help='Path to the repository root.')
-    parser.add_argument('--ignore-submodule-path',
-                        type=str,
-                        required=None,
-                        help='Comma-separated list of submodules path to ignore.')
-    parser.add_argument('--fail-on-incorrect-version',
-                        action='store_true',
-                        help='Flag to indicate script to fail for incorrect submodules versions in manifest.yml')
+                        help='Path to the manifest.yml file.')
+    parser.add_argument('-i',
+                        '--ignore-paths',
+                        action='store',
+                        dest='ignore_paths',
+                        nargs='+',
+                        default="",
+                        help='Submodules that are not listed in the mnanifest file and should be ignored.')
+    parser.add_argument('-s',
+                        '--ignore-spdx',
+                        action='store',
+                        dest='ignore_spdx',
+                        nargs='+',
+                        default="",
+                        help='SPDX identifiers which are expected and non-standard.')
 
     args = parser.parse_args()
+    return args
 
-    if args.ignore_submodule_path != None:
-        IGNORE_SUBMODULES_LIST = args.ignore_submodule_path.split(',')
+def print_error(indent_level, error_str):
+    """Print an error to standard out in red with a given indentation level"""
+    cprint('{}ERROR: {}'.format('\t' * indent_level, error_str),'red')
+
+
+def print_warning(indent_level, warn_str):
+    """Print a warning to standard out in cyan with a given indentation level"""
+    cprint('{}WARN:  {}'.format('\t' * indent_level, warn_str),'cyan')
+
+def validate_submodule_repo(dep, manifest_dir):
+    """Validate a given submodule referred to in a package manifest dependency stanza."""
+    error_count = 0
+    repo = dep["repository"]
+    submodule = git.Repo(os.path.join(manifest_dir, repo["path"]))
+
+    print("    Manifest URL:     {}".format(repo["url"]))
+    print("    Submodule URL:    {}".format(submodule.remotes.origin.url))
+
+    # URLs are case-insensitive
+    if not submodule.remotes.origin.url.lower() == repo["url"].lower():
+        print_error(1, "Repository url mismatch between manifest and submodule.")
+        error_count += 1
+
+    manifest_hash = "unknown"
+    try:
+        manifest_hash = submodule.commit(dep["version"]).hexsha
+    except gitdb.exc.BadName:
+        print_error(
+            1, 'Manifest "version" is not a valid tag or branch in the submodule.'
+        )
+
+    # Try with lowercase version
+    if manifest_hash == "unknown":
+        try:
+            manifest_hash = submodule.commit(dep["version"].lower()).hexsha
+            print_warning("Manifest version tag is valid when converted to lowercase.")
+        except gitdb.exc.BadName:
+            pass
+
+    print("    Manifest Version: {}".format(dep["version"]))
+    print("    Manifest hash:    {}".format(manifest_hash))
+    print("    Submodule hash:   {}".format(submodule.head.commit.hexsha))
+    if not submodule.head.commit.hexsha == manifest_hash:
+        print_error(1, "Revision mismatch between submodule and manifest version.")
+        error_count += 1
+    return error_count
+
+def validate_repository(dep, manifest_dir):
+    """Validate a given repository stanza from a package manifest."""
+    repo = dep["repository"]
+    repo_name = "unknown"
+    if "name" in dep:
+        repo_name = dep
+    else:
+        repo_name = str(repo)
+
+    print("Validating repo:      {}".format(repo_name))
+    error_count = 0
+    if "type" not in repo:
+        print_error(1, '"type" field not found in repository.')
+        error_count += 1
+    if "git" != repo["type"]:
+        print_error(1, '"type" field is not set to "git" in repository')
+        error_count += 1
+    if "url" not in repo:
+        print_error(1, '"url" field not found in repository')
+        error_count += 1
+    if "path" in repo:
+        if not os.path.exists(os.path.join(manifest_dir, repo["path"])):
+            print_error(1, "Relative path does not exist: {}".format(repo["path"]))
+            error_count += 1
+        # Validate submodule revision and url.
+        else:
+            error_count += validate_submodule_repo(dep, manifest_dir)
+    return error_count
+
+def load_spdx_data(ignore_spdx):
+    """Load spdx license and license exception information from github."""
+    spdx_data = dict()
+    spdx_url = "https://raw.githubusercontent.com/spdx/license-list-data/"
+
+    print("Downloading SPDX license data...")
+
+    licenses_url = spdx_url + "master/json/licenses.json"
+    licenses = requests.get(licenses_url).json()
+
+    assert "licenses" in licenses
+    spdx_data["licenses"] = dict()
+
+    for license in licenses["licenses"]:
+        spdx_data["licenses"][license["licenseId"]] = True
+
+    for license in ignore_spdx:
+        spdx_data["licenses"][license] = True
+
+    exceptions_url = spdx_url + "master/json/exceptions.json"
+    exceptions = requests.get(exceptions_url).json()
+
+    assert "exceptions" in exceptions
+
+    spdx_data["exceptions"] = dict()
+
+    for exception in exceptions["exceptions"]:
+        spdx_data["exceptions"][exception["licenseExceptionId"]] = True
+
+    return spdx_data
+
+def check_license_tag(spdx_data, cur_license):
+    """
+    Validate a given SPDX license tag against SPDX data previously fetched with
+    load_spdx_data()
+    """
+    error_count = 0
+    licenses = cur_license.split(" ")
+    license_exception_flag = False
+    paren_depth = 0
+    last_paren_depth = 0
+    for i in range(0, len(licenses)):
+        if licenses[i][0] == "(":
+            paren_depth += 1
+        # skip "and" "or" keywords
+        if licenses[i] in ["and", "AND", "or", "OR"]:
+            pass
+        # "with" keyword denotes a license exception
+        elif licenses[i] in ["with", "WITH"]:
+            # Set flag for next iteration
+            license_exception_flag = True
+        elif license_exception_flag:
+            if not licenses[i].strip("()") in spdx_data["exceptions"]:
+                print_error(
+                    0,
+                    "Invalid license exception id {} in SPDX tag {}".format(
+                        licenses[i], cur_license
+                    ),
+                )
+                error_count += 1
+            # No '(' character -> single license exception
+            if paren_depth <= last_paren_depth:
+                license_exception_flag = False
+        else:
+            if not licenses[i].strip("()") in spdx_data["licenses"]:
+                print_error(
+                    0,
+                    "Invalid license id {} in SPDX tag {}".format(
+                        licenses[i], cur_license
+                    ),
+                )
+                error_count += 1
+
+        last_paren_depth = paren_depth
+        if licenses[i][-1] == ")":
+            paren_depth -= 1
+
+    return error_count
+
+
+def list_repo_paths_in_manifest(manifest):
+    """Returns a list of submodule paths in the given manifest"""
+    repo_list = []
+    if "dependencies" in manifest:
+        dependencies = manifest["dependencies"]
+        for dep in dependencies:
+            if "repository" in dep:
+                repo = dep["repository"]
+                if "path" in repo:
+                    repo_list.append(repo["path"])
+    return repo_list
+
+def get_all_submodules(repo_root_path, ignored_paths):
+    """Returns a list of all submodules contained in repo_root_path"""
+    path_list = []
+    repo = git.Repo(repo_root_path)
+    for submodule in repo.submodules:
+        path = os.path.relpath(submodule.abspath,repo_root_path)
+        if path not in ignored_paths:
+            path_list.append(path)
+
+    return path_list
+
+def check_for_missing_submodules(manifest_repos, git_submodules):
+    """Check for any extra git submodules in this repository"""
+    error_count = 0
+    for submodule in git_submodules:
+        if submodule not in manifest_repos:
+            print_error(0, "Git submodule {} was not found in the manifest.".format(submodule))
+            error_count += 1
+    return error_count
+
+def validate_dependency(dependency, spdx_data):
+    """Validate a given dependency stanza from a package manifest."""
+    error_count = 0
+    if "name" not in dependency:
+        print_error(0, "name field not found in dependency stanza.")
+        error_count += 1
+
+    if "version" not in dependency:
+        print_error(0, "version field not found in dependency stanza.")
+        error_count += 1
+
+    if "license" not in dependency:
+        print_error(1, '"license" field not found dependency stanzay.')
+        error_count += 1
+    else:
+        print("Validating SPDX license tag: {}".format(dependency["license"]))
+        error_count += check_license_tag(spdx_data, dependency["license"])
+    return error_count
+
+def validate_manifest(manifest_path, ignored_paths, spdx_data):
+    error_count = 0
+    manifest_dir = os.path.dirname(manifest_path)
+    with open(manifest_path, "r") as file:
+        manifest = yaml.load(file, Loader=yaml.FullLoader)
+
+        if "name" not in manifest:
+            print_error(0, "name field not found in manifest root.")
+            error_count += 1
+
+        if "version" not in manifest:
+            print_error(0, "version field not found in manifest root.")
+            error_count += 1
+
+        if "description" not in manifest:
+            print_error(0, "version field not found in manifest root.")
+            error_count += 1
+
+        if "license" not in manifest:
+            print_error(0, "version field not found in manifest root.")
+            error_count += 1
+        else:
+            print("Validating SPDX license tag: {}".format(manifest["license"]))
+            error_count += check_license_tag(spdx_data, manifest["license"])
+
+        if "dependencies" in manifest:
+            dependencies = manifest["dependencies"]
+            for dep in dependencies:
+                error_count += validate_dependency(dep, spdx_data)
+
+                if "repository" in dep:
+                    error_count += validate_repository(dep, manifest_dir)
+
+        repo_paths = list_repo_paths_in_manifest(manifest)
+        submodule_paths = get_all_submodules(os.path.dirname(manifest_path), ignored_paths)
+
+        error_count += check_for_missing_submodules(repo_paths, submodule_paths)
+
+    return error_count
+
+def main():
+    args = process_commandline_args()
 
     # Convert any relative path (like './') in passed argument to absolute path.
-    REPO_PATH = os.path.abspath(args.repo_root_path)
+    manifest_path = os.path.abspath(args.manifest_path)
 
-    submodules_info_from_manifest = read_manifest()
-    submodule_path_from_manifest = [pair[0] for pair in submodules_info_from_manifest.values()]
-    submodule_path_from_manifest = sorted(submodule_path_from_manifest)
+    ignore_paths = args.ignore_paths
 
-    submodules_info_from_git = get_all_submodules()
-    submodule_path_from_git = sorted(submodules_info_from_git.keys())
+    if not os.path.exists(manifest_path):
+        print_error(0, "Error: no file found at {}".format(manifest_path))
+        sys.exit(-1)
 
-    print(REPO_PATH)
-    print('\nList of submoduled libraries being verified:', submodule_path_from_git)
+    manifest_dir = os.path.dirname(manifest_path)
 
-    # Check that manifest.yml contains entries for all submodules
-    # present in repository.
-    if submodule_path_from_manifest != submodule_path_from_git:
-        print('manifest.yml is missing entries for:')
-        # Find list of library submodules missing in manifest.yml
-        for git_path in submodule_path_from_git:
-            if git_path not in submodule_path_from_manifest:
-                print(git_path)
-        sys.exit(1)
+    for p in ignore_paths:
+        if not os.path.exists(os.path.join(manifest_dir, p)):
+            print_error(0, "Ignored path: {} was not found.".format(p))
 
-    # Verify that manifest contains correct versions of submodules pointers.
-    mismatch_flag = False
-    print('\nVerifying that manifest.yml versions are up-to-date.....')
-    for submodule_name, submodule_info in submodules_info_from_manifest.items():
-        relative_path = submodule_info[0]
-        manifest_commit =  submodule_info[1]
-        submodule = Repo(REPO_PATH+'/'+relative_path)
-        submodule.remote('origin').fetch()
-        submodule.git.checkout(manifest_commit)
-        if (submodules_info_from_git[relative_path] != submodule.head.commit):
-            print('manifest.yml does not have correct commit ID for', submodule_name,'manifest Commit=(',manifest_commit, submodule.head.commit,') Actual Commit=',submodules_info_from_git[relative_path])
-            mismatch_flag = True
-        
-    if mismatch_flag and args.fail_on_incorrect_version:
-        sys.exit(1)
+    ignore_spdx = args.ignore_spdx
 
-    print('\nmanifest.yml file has been verified!')
-    sys.exit(0)
+    spdx_data = load_spdx_data(ignore_spdx)
 
+    error_count = validate_manifest(manifest_path, ignore_paths, spdx_data)
+
+    print("Total errors: {}".format(error_count))
+
+    sys.exit(error_count > 0)
+
+if __name__ == '__main__':
+    main()
 
